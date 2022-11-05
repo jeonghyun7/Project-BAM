a977d0c6a0545a0036ca6a66dae37cf441b6b3e4
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-11-06 00:10:06 +0900

    Update NaverApi20.java

--- src/main/java/com/bam/auth/NaverApi20.java
+++ src/main/java/com/bam/auth/NaverApi20.java
@@ -2,8 +2,8 @@ package com.bam.auth;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
-//NaverApi20은 항상 싱글톤 패턴으로 만들어야 한다.
-//
+// NaverApi20은 항상 싱글톤 패턴으로 만들어야 한다.
+
 public class NaverApi20 extends DefaultApi20 implements SnsUrl{
 	
 	private NaverApi20() {}
